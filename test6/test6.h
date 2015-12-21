eb8f559f44bc45005a68a45c366c38832476b8fc
D-e-e-m-o <wsd009811@gmail.com>
2015-12-21 22:31:42 +0800

    test6

---
+++ test6/test6.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include<afxwin.h>
+#include<iostream>
+using namespace std;
+
+class CTestWnd :public CFrameWnd
+{
+	DECLARE_MESSAGE_MAP()
+public:
+	LRESULT OnPaint(WPARAM wParam, LPARAM lParam);
+	LRESULT OnLButtonDown(WPARAM wParam, LPARAM lParam);
+	LRESULT OnRButtonDown(WPARAM wParam, LPARAM lParam);
+	LRESULT OnTimer(WPARAM wParam, LPARAM lParam);
+	short m_Time;
+	CTestWnd();
+};
+
+class CTestApp :public CWinApp
+{
+public:
+	BOOL InitInstance();
+};
+
+CTestApp theApp;
